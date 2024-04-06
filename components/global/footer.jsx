690b27afd43fcba656eb5e993ac73360592c7b5c
Arsi <arsalanghoochani15@gmail.com>
2024-04-06 02:52:12 -0700

    v-0.5

--- components/global/footer.jsx
+++ components/global/footer.jsx
@@ -9,7 +9,7 @@ import { FiClock } from "react-icons/fi";
 export default function Footer() {
   return (
     <footer className='bg-gradient-to-bl from-primary-200 to-primary-100 mt-20'>
-        <div className='2xl:max-w-screen-2xl max-w-screen-xl mx-auto sm:p-5 p-2 2xl:px-24 flex flex-wrap items-start justify-between text-primary-500 text-2xl max-md:flex-col max-md:gap-y-12'>
+        <div className='2xl:max-w-screen-2xl max-w-screen-xl mx-auto sm:p-5 p-4 2xl:px-24 flex flex-wrap items-start justify-between text-primary-500 text-2xl max-md:flex-col max-md:gap-y-12'>
             <span className="flex flex-wrap gap-x-3 items-center">
                 <img className="w-44 max-sm:w-36" src="/images/cafe-logo.png" alt="cafe-logo" />
                 <p className="sm:text-4xl text-2xl text-primary-500">کـافه رستـوران میدون</p>
