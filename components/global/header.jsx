852a8f54e3fc38647c7a3016a4bdf9b2a7362d92
Arsi <arsalanghoochani15@gmail.com>
2024-04-06 02:59:28 -0700

    v-0.5

--- components/global/header.jsx
+++ components/global/header.jsx
@@ -5,9 +5,11 @@ import Logo from './logo';
 
 export default function Header() {
   return (
-    <header className='2xl:max-w-screen-2xl 2xl:px-24 max-w-screen-xl mx-auto p-3 flex justify-between items-center'>
+    <header className='2xl:max-w-screen-2xl 2xl:px-16 max-w-screen-xl mx-auto'>
+      <div className='bg-gradient-to-bl from-primary-200 to-primary-100 flex justify-between items-center sm:p-5 pt-0 sm:rounded-b-[3rem] p-3 rounded-b-3xl'>
         <Logo/>
         <Search/>
+      </div>
     </header>
   )
 }
@@ -15,9 +17,9 @@ export default function Header() {
 
 const Search = () => {
     return (
-        <div className='relative flex items-center bg-zinc-100 rounded-lg overflow-hidden pl-2 max-sm:p-3 max-sm:cursor-pointer'>
-            <input type="text" className=' p-2 bg-inherit text-xl rounded-r-lg placeholder:text-neutral-600 outline-none max-sm:hidden' placeholder='جست و جو'/>
-            <FiSearch className='text-2xl text-zinc-500'/>
+        <div className='relative flex items-center sm:bg-zinc-100 rounded-lg overflow-hidden sm:pl-2'>
+            <input type="text" className='p-2 bg-inherit text-xl rounded-r-lg placeholder:text-neutral-600 outline-none max-md:w-36 max-sm:hidden' placeholder='جست و جو'/>
+            <FiSearch className='text-2xl text-zinc-500 max-sm:text-3xl max-sm:text-primary-500 cursor-pointer'/>
         </div>
     )
 }
